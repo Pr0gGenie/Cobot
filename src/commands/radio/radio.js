fdb4a00c23cd37ce92498ce9e574c0bbfc105730
Goutham Krishna Shabu <gouthamshabu@gmail.com>
2025-01-26 10:52:30 -0700

    fixed command syntax error and added stop subcommand

--- src/commands/radio/radio.js
+++ src/commands/radio/radio.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
+const { useMainPlayer, useQueue } = require("discord-player");
 const presets = require("../../config/presets.json");
 
 module.exports = {
@@ -6,42 +7,44 @@ module.exports = {
     .setName("radio")
     .setDescription("Manage this radio bot")
     .addSubcommand((subcommand) =>
-        subcommand
-            .setName("start")
-            .setDescription("Start a preset in the current voice channel you are in")
-            .addStringOption((option) =>
-                option
-                    .setName("preset")
-                    .setDescription("Selection of presets to play")
-                    .setRequired(true)
-                    .addChoices(
-                        ...Object.keys(presets).map((name) => ({
-                            name: name,
-                            value: name,
-                        }))
-                    )
-                )
+      subcommand
+        .setName("start")
+        .setDescription("Start a preset in the current voice channel you are in")
+        .addStringOption((option) =>
+          option
+            .setName("preset")
+            .setDescription("Selection of presets to play")
+            .setRequired(true)
+            .addChoices(
+              ...Object.keys(presets).map((name) => ({
+                name: name,
+                value: name,
+              }))
             )
+        )
+    )
     .addSubcommand((subcommand) =>
-        subcommand
-            .setName("stop")
-            .setDescription("Stops the current playing preset, and disconnects the bot"))
-    .addSubcommand((subcommand) => 
-        subcommand
-            .setName("replace")
-            .setDescription("Replaces the playing preset with a different one"))
-            .addStringOption((option) =>
-                option
-                    .setName("replace_with")
-                    .setDescription("Replace with this preset")
-                    .setRequired(true)
-                    .addChoices(
-                        ...Object.keys(presets).map((name) => ({
-                            name: name,
-                            value: name,
-                        }))      
-                    )
-                ),
+      subcommand
+        .setName("stop")
+        .setDescription("Stops the current playing preset, and disconnects the bot")
+    )
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("replace")
+        .setDescription("Replaces the playing preset with a different one")
+        .addStringOption((option) =>
+          option
+            .setName("replace_with")
+            .setDescription("Replace with this preset")
+            .setRequired(true)
+            .addChoices(
+              ...Object.keys(presets).map((name) => ({
+                name: name,
+                value: name,
+              }))
+            )
+        )
+    ),
     
     // TODO: the user can control the timer the preset is played for
     // TODO: the user can choose to set it as a pomodoro timer instead
